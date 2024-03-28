7d6e05d98e958201c830516bafe775fd313c378f
0xEileen <i.garciaru.2019@alumnos.urjc.es>
2024-03-28 12:39:35 +0100

    esquema trainer & model (#8)

--- bin/server/system/model.py
+++ bin/server/system/model.py
@@ -1,12 +1,22 @@
-
+import numpy as np
+from ultralytics import YOLO
 class Model:
     def __init__(self, path):
         super().__init__()
         self._load_model(path)
+        self.model = self.define_model()
 
-    def _load_model(self, path):
+    def _load_model(self, path: str) -> YOLO:
+        return YOLO(path)
+
+    def predict(self, image: np.ndarray, model: YOLO):
+        results = model.predict(image)
+        #boxes = results[0].boxes.xyxy
         pass
 
-    def predict(self, image):
-        pass
-
+    def define_model(self) -> YOLO:
+        """
+            Define the neural network model
+        :return: YOLO
+        """
+        return YOLO('yolov8n.pt') # sin pesos 'yolov8n.yaml' con pesos 'yolov8n.pt'
--- bin/server/training/trainer.py
+++ bin/server/training/trainer.py
@@ -1,3 +1,9 @@
+from utils.plots import plot_results
+from ultralytics import YOLO
+import sys
+
+sys.path.append('../server/system')
+import model
 
 class Trainer:
     def __init__(self):
@@ -5,19 +11,28 @@ class Trainer:
         self.model = self._init()
 
     def _init(self):
-        pass
+        return model.define_model()
 
     def compute_metrics(self, labels, predictions):
         pass
 
-    def train(self):
-        pass
+    def train(self, model: YOLO, data: yaml, epochs: int, width: int, height: int):
+        model.train(data=data, epochs=epochs, imgsz=(width,height))
+        model.val()
+        model.export(format='onnx')  # export the model to ONNX format
 
     def evaluate(self):
+        model.val()
         pass
 
-    def save_model(self, path):
-        pass
+    def save_model(self, model: YOLO):
+        model.export(format='onnx')
 
-    def plot_loss(self, loss_history):
-        pass
+    def plot_loss(self, path: str):
+        # https://docs.ultralytics.com/es/yolov5/tutorials/train_custom_data/#local-logging
+        plot_results(path)  # plot 'results.csv' as 'results.png' -> 'path/to/results.csv')
+        # if logger == 'TensorBoard':
+        #     %load_ext
+        #     tensorboard
+        #     %tensorboard - -logdir
+        #     runs / train
