8d9aa5580b56d6b5b603847ce58fe0da459b7b08
0xEileen <i.garciaru.2019@alumnos.urjc.es>
2024-03-31 18:43:43 +0200

    Arquitectura Yolov8 (#8)

--- bin/server/system/model.py
+++ bin/server/system/model.py
@@ -1,22 +1,44 @@
-import numpy as np
+import matplotlib.pyplot as plt
 from ultralytics import YOLO
+import numpy as np
+import cv2
+
+
 class Model:
-    def __init__(self, path):
+    def __init__(self):
         super().__init__()
-        self._load_model(path)
         self.model = self.define_model()
 
-    def _load_model(self, path: str) -> YOLO:
-        return YOLO(path)
+    def _load_model(self, path: str):
+        """
+        Load a YOLO model from the specified path.
 
-    def predict(self, image: np.ndarray, model: YOLO):
-        results = model.predict(image)
-        #boxes = results[0].boxes.xyxy
-        pass
+        :param path: str, Path to the YOLO model.
+        :return: None
+        """
+        self.model = YOLO(path)
+
+    def predict(self, image: np.ndarray):
+        """
+        Predict the bounding box of the given image
+
+        :param image: np.ndarray, Image to be predicted
+        :return: None
+        """
+        results = self.model.predict(image)
+        for i in results:
+            box = i.boxes.xyxy
+            print(box)
+            if box.size != 0:
+                img = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
+                plt.imshow(img)
+                plt.axis('off')
+                plt.show()
 
     def define_model(self) -> YOLO:
         """
-            Define the neural network model
-        :return: YOLO
+        Define and initialize the neural network model.
+
+        :return: YOLO, Initialized YOLO model.
         """
-        return YOLO('yolov8n.pt') # sin pesos 'yolov8n.yaml' con pesos 'yolov8n.pt'
+        return YOLO('yolov8n.pt')  # sin pesos 'yolov8n.yaml' con pesos 'yolov8n.pt'
--- bin/server/training/trainer.py
+++ bin/server/training/trainer.py
@@ -1,38 +1,106 @@
-from utils.plots import plot_results
+import matplotlib.pyplot as plt
 from ultralytics import YOLO
+import cv2
 import sys
 
-sys.path.append('../server/system')
-import model
+sys.path.append('../system')
+from model import Model
+
 
 class Trainer:
     def __init__(self):
+        """
+        Initializes the Trainer class.
+        """
         super().__init__()
-        self.model = self._init()
 
-    def _init(self):
-        return model.define_model()
+    def train(self, model: YOLO, data: str, epochs: int, batch: int, size: int, name: str, path: str):
+        """
+        Train the specified YOLO model.
 
-    def compute_metrics(self, labels, predictions):
-        pass
+        :param model: YOLO,  model to train.
+        :param data: str, Path to the dataset YAML file.
+        :param epochs: int, Number of epochs for training.
+        :param batch: int, Batch size for training.
+        :param size: int, Image size (e.g., 488x442).
+        :param name: str, Name of the results directory.
+        :param path: str, Directory for saving models, results & logging.
+        :return: None
+        """
+        results = model.train(
+            data=data,
+            imgsz=size,
+            epochs=epochs,
+            batch=batch,
+            name=name,
+            project=path)
 
-    def train(self, model: YOLO, data: yaml, epochs: int, width: int, height: int):
-        model.train(data=data, epochs=epochs, imgsz=(width,height))
-        model.val()
-        model.export(format='onnx')  # export the model to ONNX format
+        print("Train metrics: ")
+        for key, value in results.results_dict.items():
+            if key.startswith('metrics/'):
+                key = key[len('metrics/'):]
+            print(f"{key}: {value:.6f}")
 
-    def evaluate(self):
-        model.val()
-        pass
+    def evaluate(self, model: YOLO, name: str, path: str):
+        """
+        Evaluates the trained YOLO model.
 
-    def save_model(self, model: YOLO):
-        model.export(format='onnx')
+        :param model: YOLO, The trained model to be evaluated.
+        :param name: str, Name of the results directory.
+        :param path: str, Directory for saving models, results & logging.
+        :return: None
+        """
+        result = model.val(name=name, project=path)
+        print("Validation metrics: ")
+        for key, value in result.results_dict.items():
+            if key.startswith('metrics/'):
+                key = key[len('metrics/'):]
+            print(f"{key}: {value:.6f}")
+
+    def save_model(self, model: YOLO, format: str, name: str, path: str):
+        """
+        Saves the trained YOLO model in the specified format.
+
+        :param model: YOLO, The trained model to be saved.
+        :param format: str, The format to export the model, such as ONNX format or torchscript.
+        :param name: str, Name of the results directory.
+        :param path: str, Directory for saving models, results & logging.
+        :return: None
+        """
+        model.export(format=format, name=name, project=path)
 
     def plot_loss(self, path: str):
-        # https://docs.ultralytics.com/es/yolov5/tutorials/train_custom_data/#local-logging
-        plot_results(path)  # plot 'results.csv' as 'results.png' -> 'path/to/results.csv')
-        # if logger == 'TensorBoard':
-        #     %load_ext
-        #     tensorboard
-        #     %tensorboard - -logdir
-        #     runs / train
+        """
+        Plot the loss curve.
+
+        :param path: str, Path to the loss curve image (str).
+        :return: None
+        """
+        img = cv2.imread(path)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
+
+
+def main():
+    save_path = './my_train'
+    save_val = './my_val'
+    name = 'Yolo_Training'
+
+    trainer = Trainer()
+    model_ = Model()
+    trainer.train(model_.model, '../../../dataset/data.yaml', 1, 8, 512, name, save_path)
+    print("Training finished!")
+
+    model_._load_model(f'{save_path}/{name}/weights/best.pt')
+
+    trainer.evaluate(model_.model, name, save_val)
+    trainer.save_model(model_.model, 'torchscript', name, save_path)
+    trainer.plot_loss(f'{save_path}/{name}/results.png')
+
+    img = cv2.imread('../../../dataset/MuestrasMaderas/26.png')
+    model_.predict(img)
+
+
+if __name__ == "__main__":
+    main()
