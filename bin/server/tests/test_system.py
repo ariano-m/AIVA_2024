97d2bfca66332356fe76e0b85ef91173262fadb1
0xEileen <i.garciaru.2019@alumnos.urjc.es>
2024-04-05 21:11:37 +0200

    Test sistema (#23)

--- bin/server/tests/test_system.py
+++ bin/server/tests/test_system.py
@@ -13,32 +13,54 @@ class test_system(TestCase):
     model = Model("./model/")
     my_system = System(model)
     image = cv.imread("./images/49.png")
-    figures_coords = {'circle': [-50, 8, 500], 'triangle': [1, 40, 5, 90, 8], 'reactangle': [9, 15, -20, 100]}
-    damage_coords = []
-    path = './output'
+    path = './output.png'
+
+    def test_morphology(self):
+        self.assertEqual(self.my_system.preprocess_image(self.image).shape, (442, 488, 3))
+
+    def test_get_contours(self):
+        coords = self.my_system.get_contours(self.image)
+        self.assertTrue(isinstance(coords, cv.typing.Rect))  # Sequence[int]
+        x, y, w, h = coords
+        self.assertTrue(0 <= x < self.image.shape[0])
+        self.assertTrue(0 <= y < self.image.shape[1])
+        self.assertTrue(0 <= x + w < self.image.shape[0])
+        self.assertTrue(0 <= y + h < self.image.shape[1])
 
     def test_preprocess_image(self):
         self.assertEqual(self.my_system.preprocess_image(self.image).size(), (488, 442))
 
     def test_detect_damage(self):
-        coords = self.model.predict(self.image)
+        coords = self.my_system.detect_damage(self.image)
         self.assertTrue(isinstance(coords, list))
         self.assertEquals(len(coords) % 2, 0)
 
     def test_place_figures(self):
-        # {'circle': [x, y, radio], 'triangle': [x1, y1, x2, y2, x3, y3], 'rectangle': [x, y, h, w]}
-        result = self.my_system.place_figures(self.image, self.damage_coords)
-        self.assertEquals(len(result['circle']), 2)
-        self.assertEquals(len(result['triangle']), 6)
-        self.assertEquals(len(result['rectangle']), 4)
+        img_bin = self.my_system.preprocess_image(self.image)
+        erode_img = self.my_system.morphology(img_bin)
+        result = self.my_system.place_figures(self.image,
+                                              self.my_system.detect_damage(self.image),
+                                              self.my_system.get_contours(erode_img))
+
+        for x, y, x2, y2 in result:
+            self.assertTrue(0 <= x < self.image.shape[0])
+            self.assertTrue(0 <= y < self.image.shape[1])
+            self.assertTrue(0 <= x2 < self.image.shape[0])
+            self.assertTrue(0 <= y2 < self.image.shape[1])
 
     def test_color_figures(self):
-        result = self.my_system.color_figures(self.image, self.figures_coords)
-        self.assertTrue(isinstance(result, np.ndarray))
-        with self.assertRaises(TypeError):
-            self.my_system.color_figures(self.image, self.figures_coords)
+        img_bin = self.my_system.preprocess_image(self.image)
+        erode_img = self.my_system.morphology(img_bin)
+        coord = self.my_system.place_figures(self.image,
+                                               self.my_system.detect_damage(self.image),
+                                               self.my_system.get_contours(erode_img))
 
-    def test_save_image(self):
-        self.assertTrue(os.path.exists(self.path))
-        with self.assertRaises(TypeError):
-            self.my_system.save_image(self.image, self.path)
+        result = self.my_system.color_figures(self.image, coord)
+        self.assertTrue(isinstance(result, np.ndarray))
+        #with self.assertRaises(TypeError):
+        #    self.my_system.color_figures(self.image, coord)
+
+    # def test_save_image(self):
+    #     self.assertTrue(os.path.exists(self.path))
+    #     with self.assertRaises(TypeError):
+    #          self.my_system.save_image(self.image, self.path)
